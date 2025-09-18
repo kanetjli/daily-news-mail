02dc3bcdaf969c8d7fb5e5ef16d2bca6da8d5a08
kane <49839105+kanetjli@users.noreply.github.com>
2025-09-18 22:47:29 +0800

    Update send_news.py

--- send_news.py
+++ send_news.py
@@ -1,11 +1,12 @@
+# send_news.py
 import os, smtplib, requests, datetime
 from email.mime.text import MIMEText
 
-KEY = "89b89f6d8ca841ac98ad1dbe6af33d8b"
-url = f'https://newsapi.org/v2/top-headlines?country=cn&apiKey={KEY}&pageSize=5'
+KEY = '89b89f6d8ca841ac98ad1dbe6af33d8b'          # 你的 NewsAPI 密钥
+url = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=' + KEY + '&pageSize=5'
 data = requests.get(url, timeout=10).json()
 titles = [n['title'] for n in data['articles']]
-body = '今日国内头条：\n\n' + '\n'.join(f'• {t}' for t in titles)
+body = 'BBC 今日头条：\n\n' + '\n'.join(f'• {t}' for t in titles)
 
 msg = MIMEText(body, 'plain', 'utf-8')
 msg['Subject'] = f'GitHub 每日新闻 {datetime.date.today()}'
